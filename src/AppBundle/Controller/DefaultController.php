fcc8f305678a0e7649f90dfc60cbf48761e1225b
Sharupich, Vladislav <v.sharupich@itransition.com>
2021-05-28 18:30:29 +0300

    DEVELop

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,7 +11,8 @@ class DefaultController extends Controller
 
     public function indexAction(Request $request)
     {
-        $locale = $request->getLocale();
+//        $request->setLocale('fr');
+        var_dump($request->getLocale());
 //        dump($locale);
         return $this->render('@App/test.html.twig');
     }
